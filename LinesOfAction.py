ce80feef4c3c29b1e0196fe5927ee325415e8cbe
Knovnov <notknovus@gmail.com>
2023-11-13 11:22:37 -0800

    added states but broken atm

--- LinesOfAction.py
+++ LinesOfAction.py
@@ -9,20 +9,23 @@ class LinesOfAction:
     def __init__(self, SIZE = 10):
         self.board = Board(SIZE)
         self.SIZE = SIZE
+        self.SIDE = 1.0/SIZE
+        self.HALF = self.SIDE/2.0
+        self.state = "idle"
 
-    def display(self):
-        """draws a frame of the board"""
+    def display(self, state):
+        """draws a frame of the board in a given state"""
         stddraw.clear()
 
         #a few definitions
         size = self.SIZE
-        side = 1.0/size
-        half = side/2.0
+        side = self.SIDE
+        half = self.HALF
 
-        #draw the board
         #blue background
         stddraw.setPenColor(stddraw.BOOK_LIGHT_BLUE)
         stddraw.filledSquare(0, 0, 1)
+        #draw the board
         for x in range(size):
             for y in range(size):
                 #get a specifc square of the board
@@ -30,21 +33,50 @@ class LinesOfAction:
                 #if there's a piece on the square
                 if (square != None):
                     #draw white piece
-                    if square.team == "white":
+                    if square.getTeam() == "white":
                         stddraw.setPenColor(stddraw.WHITE)
-                        stddraw.filledCircle(x*side + half, y*side + half, half/0.5*0.3)
+                        stddraw.filledCircle(x*side + half, y*side + half, half/0.5*0.4)
                     #draw black piece
                     else:
                         stddraw.setPenColor(stddraw.BLACK)
-                        stddraw.filledCircle(x*side + half, y*side + half, half/0.5*0.3)
+                        stddraw.filledCircle(x*side + half, y*side + half, half/0.5*0.4)
                 #draw black borders
                 stddraw.setPenColor(stddraw.BLACK)
                 stddraw.square(x*side + half, y*side + half, half)
+                
+        if state == "picking":
+            xx = stddraw.mouseX()
+            yy = stddraw.mouseY()
+            x = round((xx - half)/side)
+            y = round((yy - half)/side)
+
+            #highlights the currently picked piece
+            if self.board.getBoard()[x][y] != None:
+                stddraw.setPenColor(stddraw.YELLOW)
+                stddraw.filledCircle(x*side + half, y*side + half, half/0.5*0.5)
+                stddraw.setPenColor(stddraw.BLACK)
+                stddraw.filledCircle(x*side + half, y*side + half, half/0.5*0.4)
+        if state == "moving":
+            xx = stddraw.mouseX()
+            yy = stddraw.mouseY()
+            x = round((xx - half)/side)
+            y = round((yy - half)/side)
+
+            self.board.getBoard()[x][y] = None
+            self.state = "idle"
         #idfk
         stddraw.show(0)
     
     def play(self):
-        self.display()
+        #condition from picking -> moving
+        if stddraw.mousePressed() and self.state == "picking":
+            self.state = "moving"
+        #condition from idle -> picking
+        if stddraw.mousePressed() and self.state == "idle":
+            self.state = "picking"
+
+        self.display(self.state)
+        
 
 
 
