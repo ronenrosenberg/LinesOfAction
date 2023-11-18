f1878ccdfd8365b9fbdf897c55ab3afb6f3c3619
Knovnov <notknovus@gmail.com>
2023-11-18 13:14:22 -0800

    enforces valid moves now

--- LinesOfAction.py
+++ LinesOfAction.py
@@ -18,8 +18,9 @@ class LinesOfAction:
         self.state = "idle"
         self.selected = None
         self.currentTeam = "black"
+        self.legalJumps = []
 
-    def display(self, state):
+    def display(self):
         """draws a frame of the board in a given state"""
         stddraw.clear()
 
@@ -50,26 +51,27 @@ class LinesOfAction:
                 stddraw.square(x*side + half, y*side + half, half)
                 
         #as long as state isn't idle, will grab click location
-        if state != "idle":
+        if self.state != "idle":
             x = round((stddraw.mouseX() - half)/side)
             y = round((stddraw.mouseY() - half)/side)
+
         #state activated when there's been a click
-        if state == "picking":
-            highlighted = self.board.getBoard()[x][y]
-            if  highlighted != None and highlighted.getTeam() == self.currentTeam: #makes sure that clicked square has a piece in it
+        if self.state == "picking":
+            atClickLocation = self.board.getBoard()[x][y]
+            if  atClickLocation != None and atClickLocation.getTeam() == self.currentTeam: #makes sure that clicked square has a piece in it
                 #highlights piece
                 stddraw.setPenColor(stddraw.YELLOW)
                 stddraw.filledCircle(x*side + half, y*side + half, half/0.5*0.5)
                 #draw white piece
-                if highlighted.getTeam() == "white":
+                if atClickLocation.getTeam() == "white":
                     stddraw.setPenColor(stddraw.WHITE)
                     stddraw.filledCircle(x*side + half, y*side + half, half/0.5*0.4)
                 else: #otherwise draw the black piece
                     stddraw.setPenColor(stddraw.BLACK)
                     stddraw.filledCircle(x*side + half, y*side + half, half/0.5*0.4)
                 
-                coords = self.board.range(highlighted)
-                for coord in coords:
+                self.legalJumps = self.board.range(atClickLocation)
+                for coord in self.legalJumps:
                     (x, y) = coord
                     stddraw.setPenColor(stddraw.YELLOW)
                     stddraw.filledSquare(x*side + half, y*side + half, half * 0.93)
@@ -81,37 +83,51 @@ class LinesOfAction:
                             stddraw.setPenColor(stddraw.BLACK)
                             stddraw.filledCircle(x*side + half, y*side + half, half/0.5*0.4)
 
-                
                 #piece is "selected"
-                self.selected = highlighted
+                self.selected = atClickLocation
 
             else: #if no piece has been selected, go back to idle
                 self.state = "idle"
-
-                
+    
         #state activated on a second click, to move the piece to the second click
-        if state == "moving":
-            self.board.move(self.selected, (x, y))
-            self.selected = None
-
-            #reset state
-            self.state = "idle"
-
-            #flips to opposite team's turn
-            if self.currentTeam == "black":
-                self.currentTeam = "white"
+        if self.state == "moving":
+            jumpLocation = self.board.getBoard()[x][y]
+            #if just clicking to a different piece on the same team, reset to picking state with that piece
+            if (jumpLocation != None) and (jumpLocation.isSameTeam(self.selected)):
+                self.selected = self.board.getBoard()[x][y]
+                self.state = "picking"
+            #if clicking on non-valid location, resets back to idle
+            elif (x, y) not in self.legalJumps:
+                #resets logic variables, goes back to idle
+                self.state = "idle"
+                self.selected = None
+                self.legalJumps = []
+            #otherwise move the piece and reset to idle
             else:
-                self.currentTeam = "black"
+                self.board.move(self.selected, (x, y))
+                #resets logic variables
+                self.state = "idle"
+                self.selected = None
+                self.legalJumps = []
+
+                #flips to opposite team's turn
+                if self.currentTeam == "black":
+                    self.currentTeam = "white"
+                else:
+                    self.currentTeam = "black"
         
         #don't question this line, it just makes it work
         stddraw.show(0)
     
     #checks for win state
     def won(self):
+        #gets total # of black and white pieces on board
         numWhite, numBlack = self.board.teamTotalPieces()
+        #checks if total # of pieces of a team equals # in a contiguous bunch
         whiteWon = numWhite == self.board.findConnectedPieceSize("white")
         blackWon = numBlack == self.board.findConnectedPieceSize("black")
         
+        #win states
         if whiteWon and blackWon:
             print("Tie")
             return True
@@ -123,9 +139,6 @@ class LinesOfAction:
             return True
         
         return False
-
-        
-        
 
     def play(self):
         #returns True if there has been a click since last time function was called
@@ -139,13 +152,13 @@ class LinesOfAction:
             self.state = "picking"
 
         print(self.state)
-        self.display(self.state)
+
+        #displays the current game state
+        self.display()
 
         if self.won():
             quit()
         
-
-
 
 if __name__ == "__main__":
     game = LinesOfAction(SIZE)
